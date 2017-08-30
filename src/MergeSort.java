a2e7007dbef26fd80c7f4b839bda1dd11de6dfbc
Travis Clarke <travis.m.clarke@gmail.com>
2017-08-29 18:48:47 -0700

    add MergeSort helper

---
+++ src/MergeSort.java
@@ -0,0 +1,51 @@
+
+public class MergeSort {
+
+    private int[] array;
+
+    public void sort(int[] values) {
+        this.array = values;
+        mergesort(0, values.length - 1);
+    }
+
+    private void mergesort(int low, int high) {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            mergesort(low, mid);
+            mergesort(mid + 1, high);
+            merge(low, mid, high);
+        }
+    }
+
+    private void merge(int low, int mid, int high) {
+
+        int p = low;
+        int q = mid + 1;
+
+        int[] _array = new int[high - low + 1];
+
+        int k = 0;
+        for (int i = low; i <= high; i++) {
+            if (p > mid) {
+                _array[k] = array[q];
+                q++;
+            } else if (q > high) {
+                _array[k] = array[p];
+                p++;
+            } else if (array[p] <= array[q]) {
+                _array[k] = array[p];
+                p++;
+            } else if (array[q] <= array[p]) {
+                _array[k] = array[q];
+                q++;
+            }
+            k++;
+        }
+
+        for (int i = 0; i < k; i++) {
+            array[low] = _array[i];
+            low++;
+        }
+    }
+
+}
