297461b443fb63209150612eb177b6dd6b40996c
Travis Clarke <travis.m.clarke@gmail.com>
2017-08-29 18:49:21 -0700

    add Node helper

---
+++ src/Node.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private int position;
+    private List<Integer> adjacents = new ArrayList<>();
+    private int distance = -1;
+    private boolean visited = false;
+
+    public Node(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public List<Integer> getAdjacents() {
+        return adjacents;
+    }
+
+    public void addAdjacent(int adjacent) {
+        this.adjacents.add(adjacent);
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+}
