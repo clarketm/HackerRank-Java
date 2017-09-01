3908f49f0f1bef26a5b476fafdb808005897370f
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-01 11:57:05 -0700

    add BubbleSort solution

---
+++ src/solutions/CrackingTheCodingInterview/BubbleSort.java
@@ -0,0 +1,47 @@
+package solutions.CrackingTheCodingInterview;
+
+import static helpers.SortUtils.swap;
+
+import java.util.Scanner;
+
+// HackerRank
+// https://www.hackerrank.com/challenges/ctci-bubble-sort
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        sort(a);
+    }
+
+    static void sort(int[] array) {
+        int numSwaps = 0;
+        int len = array.length;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                    numSwaps++;
+                }
+            }
+        }
+
+        int firstElement = array[0];
+        int lastElement = array[len - 1];
+
+        printResult(numSwaps, firstElement, lastElement);
+    }
+
+    static void printResult(int numSwaps, int firstElement, int lastElement) {
+        System.out.println(String.format("Array is sorted in %d swaps.", numSwaps));
+        System.out.println(String.format("First Element: %d", firstElement));
+        System.out.println(String.format("Last Element: %d", lastElement));
+    }
+
+}
