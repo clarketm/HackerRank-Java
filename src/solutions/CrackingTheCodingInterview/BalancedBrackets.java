70e4d9285da1dbd6d2e98789d61a862c09df0719
Travis Clarke <travis.m.clarke@gmail.com>
2017-08-31 17:30:53 -0700

    add BalancedBrackets solution

---
+++ src/solutions/CrackingTheCodingInterview/BalancedBrackets.java
@@ -0,0 +1,52 @@
+package solutions.CrackingTheCodingInterview;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedBrackets {
+    private static Map<Character, Character> matches = new HashMap<>();
+
+    public static void main(String[] args) {
+        matches.put('{', '}');
+        matches.put('(', ')');
+        matches.put('[', ']');
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String expression = in.next();
+            System.out.println((isBalanced(expression)) ? "YES" : "NO");
+        }
+    }
+
+    private static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        boolean isBalanced = true;
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            switch (ch) {
+                case '{':
+                case '(':
+                case '[':
+                    stack.push(matches.get(ch));
+                    isBalanced = false;
+                    break;
+                case '}':
+                case ')':
+                case ']':
+                    if (stack.empty() || stack.pop() != ch) {
+                        return false;
+                    }
+                    isBalanced = true;
+                    break;
+            }
+        }
+        return isBalanced;
+    }
+
+}
