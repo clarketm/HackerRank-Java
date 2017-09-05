c228bbcdeb3292fa29bf3a41e5078d0c34575bd8
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-05 11:15:27 -0700

    add LetsReview solution

---
+++ src/solutions/LetsReview.java
@@ -0,0 +1,33 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class LetsReview {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s;
+        for (int i = 0; i < n; i++) {
+            s = in.next();
+            printEvenPrefix(s);
+
+        }
+    }
+
+    static void printEvenPrefix(String s) {
+        StringBuilder even = new StringBuilder();
+        StringBuilder odd = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                even.append(s.charAt(i));
+            } else {
+                odd.append(s.charAt(i));
+            }
+        }
+
+        System.out.printf("%s %s\n", even.toString(), odd.toString());
+    }
+
+}
