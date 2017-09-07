175ae086c84693517de1b82e526932e07562987f
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-07 13:56:46 -0700

    add Recursion solution

---
+++ src/solutions/Recursion.java
@@ -0,0 +1,24 @@
+package solutions;
+
+import java.util.Scanner;
+
+// HackerRank
+// https://www.hackerrank.com/challenges/30-recursion/problem
+
+public class Recursion {
+
+    static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int result = factorial(n);
+        System.out.println(result);
+    }
+
+}
