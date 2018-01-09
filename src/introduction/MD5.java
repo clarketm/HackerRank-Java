cef67120ad948d79ca8e45d0bab1e0edf631681f
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-09 11:24:05 -0800

    add MD5.java solution

---
+++ src/introduction/MD5.java
@@ -0,0 +1,25 @@
+package introduction;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+// Java MD5
+// https://www.hackerrank.com/challenges/java-md5/problem
+
+public class MD5 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(s.getBytes());
+        byte[] digest = md5.digest();
+        String hash = DatatypeConverter.printHexBinary(digest).toLowerCase();
+
+        System.out.println(hash);
+    }
+
+}
